e04ff5bfa3bdf5545c6905100196c8986e0f09a5
MaryantoCin <firevolt719@gmail.com>
2019-12-13 20:34:55 +0700

    added team database

---
+++ database/migrations/2019_12_13_132126_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('member_id1');
+            $table->unsignedBigInteger('member_id2');
+            $table->unsignedBigInteger('member_id3');
+            $table->string('team_email');
+            $table->string('team_password');
+            $table->string('payment_image');
+            $table->boolean('payment_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
