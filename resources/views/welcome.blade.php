add2709f89d8173b40711cc79583a6c9fd1bd4bb
MaryantoCin <firevolt719@gmail.com>
2019-12-29 16:30:05 +0700

    added register but html broken-er

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,8 +102,25 @@
       <div class="modal-title">TEAM</div>
       <form method="POST" action="{{ route('register') }}">
        @csrf
+       <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+       @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+       @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
        <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
